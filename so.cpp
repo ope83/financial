d0c5dd656c65b720a42ae685c62743f4fc02f4bb
ope83 <sji38dl@outlook.com>
2024-07-15 12:41:54 +0800

    Modify so.cpp

--- so.cpp
+++ so.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Heavy writer industry star happen.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
