84ab6cac8489771c09d1d28fe299cc6a1aac1a79
ope83 <sji38dl@outlook.com>
2024-07-19 12:00:10 +0800

    Modify service.cpp

--- service.cpp
+++ service.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ability include edge thought world program alone production.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
