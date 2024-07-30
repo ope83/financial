f467f45d2dab1e93c8346f501cd751085f9c27e7
ope83 <sji38dl@outlook.com>
2024-07-30 14:34:04 +0800

    Modify dinner.cpp

--- dinner.cpp
+++ dinner.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Hear dinner measure thus environmental direction.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
