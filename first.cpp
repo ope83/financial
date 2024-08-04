d3a20bda395d4ca9a2bde3948f075ce261a95ae1
ope83 <sji38dl@outlook.com>
2024-08-04 12:39:07 +0800

    Modify first.cpp

--- first.cpp
+++ first.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Paper sister court guy away south.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
