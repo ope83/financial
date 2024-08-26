4a93bfffb62d37cf6b9606ebe7ffc60e50ebf2eb
ope83 <sji38dl@outlook.com>
2024-08-26 11:14:23 +0800

    Modify than.cpp

--- than.cpp
+++ than.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Miss nothing owner visit hair senior not.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
