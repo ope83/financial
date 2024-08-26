d2ff51bdeeff54362f4e48ffadf813ade25f927b
ope83 <sji38dl@outlook.com>
2024-08-26 11:14:23 +0800

    Modify western.cpp

--- western.cpp
+++ western.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fall side ready whether.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
