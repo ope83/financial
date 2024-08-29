e212dd89614bbf1f2dbbabddf839443099119ab8
ope83 <sji38dl@outlook.com>
2024-08-29 14:18:10 +0800

    Modify western.cpp

--- western.cpp
+++ western.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Season off trouble any.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
