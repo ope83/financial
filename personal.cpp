e14e25dd5a2b7b3b6449594ab6d36360c0a4f983
ope83 <sji38dl@outlook.com>
2024-10-15 10:53:07 +0800

    Modify personal.cpp

--- personal.cpp
+++ personal.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Establish police collection citizen prepare subject.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
