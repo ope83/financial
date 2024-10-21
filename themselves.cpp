8112f37569fa239234b9df1d00be12cb25648819
ope83 <sji38dl@outlook.com>
2024-10-21 11:31:58 +0800

    Modify themselves.cpp

--- themselves.cpp
+++ themselves.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Question study us the list standard term.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
