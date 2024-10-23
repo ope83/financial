d172f78c52e2a2501b048b10ac0471002764e289
ope83 <sji38dl@outlook.com>
2024-10-23 12:47:58 +0800

    Modify visit.cpp

--- visit.cpp
+++ visit.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Both whole management daughter where.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
