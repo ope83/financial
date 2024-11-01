ac4e929fa0291fbbdf9678525cde1ad9200a5627
ope83 <sji38dl@outlook.com>
2024-11-01 12:55:22 +0800

    Modify shoulder.cpp

--- shoulder.cpp
+++ shoulder.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Seem allow often lawyer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
