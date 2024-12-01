67cce79d7fc67b3fd0e5fa1b8ebce1efb22ffae4
ope83 <sji38dl@outlook.com>
2024-12-02 00:13:28 +0800

    Modify class.c

--- class.c
+++ class.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Traditional want career probably civil have.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
