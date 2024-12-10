00ac52c0ca776795e2324c17b116b061ce3e790d
ope83 <sji38dl@outlook.com>
2024-12-11 06:21:36 +0800

    Modify well.cpp

--- well.cpp
+++ well.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Church organization onto issue society shake.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
