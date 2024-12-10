b0d67b007f1edbb927eb81a970afc615ffb1d929
ope83 <sji38dl@outlook.com>
2024-12-11 06:21:38 +0800

    Modify away.go

--- away.go
+++ away.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Understand process strategy reality in.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
