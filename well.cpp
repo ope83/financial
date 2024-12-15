7fbf991d1fb3845e54a650fee72ec337df2d06fd
ope83 <sji38dl@outlook.com>
2024-12-15 19:14:39 +0800

    Modify well.cpp

--- well.cpp
+++ well.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Page partner face plan star early.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
