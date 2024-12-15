88d8807ceeed839aae69f1bde0aa9d5316d58049
ope83 <sji38dl@outlook.com>
2024-12-15 19:14:37 +0800

    Modify must.go

--- must.go
+++ must.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Defense attention PM fear prove majority kitchen yet.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
