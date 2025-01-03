1823dea143e687f2d14503036e5ea9571f775008
ope83 <sji38dl@outlook.com>
2025-01-03 16:40:35 +0800

    Modify common.cpp

--- common.cpp
+++ common.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Really learn bad city then.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
