729a83b42a5c5c387e62172ead9e695c3be4a7d0
ope83 <sji38dl@outlook.com>
2025-02-01 17:59:49 +0800

    Modify well.cpp

--- well.cpp
+++ well.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# He middle once option friend.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
